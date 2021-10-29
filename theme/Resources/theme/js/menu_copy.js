4a032a52c24a204107a12a39e16655474587db90
taku-ni4 <taku_ni4@me.com>
2021-10-29 18:33:25 +0900

    Delete theme/Resources/theme directory

--- theme/Resources/theme/js/menu_copy.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-	// ツリーをスマホ用レフトメニューにコピー
-	$(".op-contents-left").clone(true).appendTo($(".op-overlay-menu-tree"));
-	// 検索フォームをスマホ用レフトメニューにコピー
-	$(".op-header-search").clone(true).appendTo($(".op-overlay-menu-search"));
-});
